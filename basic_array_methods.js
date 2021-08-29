dd66077579516a3613ba97d2d05c506263686d60
a-sh <iam.angie@ymail.com>
2021-08-29 22:04:40 +1000

    Add basic_array_methods.js

---
+++ basic_array_methods.js
@@ -0,0 +1,22 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+// remove the last string of the array
+secretMessage.pop();
+
+// add the words to and Program as separate strings to the end of the secretMessage array
+secretMessage.push('to', 'Program');
+
+// array[oldWordIndex] = 'newWord';
+secretMessage[7] = 'right';
+
+secretMessage.shift();
+
+// array.unshift('newWord');
+secretMessage.unshift('Programming');
+
+// remove the strings get, right, the, first, time,, and replace them with the single string know,
+// array.splice(indexToStart, numberOfIndices, 'stringToAdd');
+secretMessage.splice(6,5, 'know,');
+
+// On one line, use console.log() and .join() to print the secret message as a sentence.
+console.log(secretMessage.join(' '));
