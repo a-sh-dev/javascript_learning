ac20f3a0b09da460aa1b2fbdc5a09bd39a05e82e
a-sh <iam.angie@ymail.com>
2021-08-31 22:12:51 +1000

    Modify basic_loops_while.js

--- basic_loops_while.js
+++ basic_loops_while.js
@@ -34,6 +34,17 @@ while (number >= 0) {
 // display the sum
 console.log(`The sum is ${sum}.`);
 
+// * From CodeCademy: 
+// Create a while loop with a condition that checks if the currentCard does not have 'spade'. Use random to generate random index to pick the card from cards until it finds spade
+
+const cards = ['diamond', 'spade', 'heart', 'club'];
+let currentCard;
+
+while (currentCard != 'spade') {
+  currentCard = cards[Math.floor(Math.random() * 4)];
+  console.log(currentCard);
+}
+
 // ! DO...WHILE LOOP
 // *Basic syntax
 // The only difference between do...while and while loop
