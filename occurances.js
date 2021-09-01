2296e079f6a1c362d28302404887ac626ea65d4d
a-sh <iam.angie@ymail.com>
2021-09-01 16:28:07 +1000

    Add occurances.js - count occurances in an array to object

---
+++ occurances.js
@@ -0,0 +1,35 @@
+let fruits = ['apple', 'apple', 'grapes', 'orange', 'orange', 'durian', 'grapes', 'apple'];
+let basket = {};
+
+// * Traditional solution
+for (let fruit of fruits) {
+  if (basket[fruit] == null) {
+    basket[fruit] = 1 
+  } else {
+    basket[fruit] += 1
+  }
+}
+
+// ! Shorter solution
+for (let fruit of fruits) {
+  // * If basket[fruit] is null, set it to 0
+  basket[fruit] = basket[fruit] ?? 0;
+  // * Increase basket[fruit] by 1
+  basket[fruit]++;
+}
+
+// ! Oneliner
+for (let fruit of fruits) {
+  basket[fruit] = (basket[fruit] || 0) + 1;
+}
+
+for (let fruit of fruits) {
+  basket[fruit] = basket[fruit] ? basket[fruit] + 1 : 1
+}
+
+for (let fruit of fruits) {
+  // ~ turn null to be 0 or 1 and added with math stuff with the - (this is hard to read!)
+  basket[fruit]=-~basket[fruit];
+}
+
+console.log(basket);
