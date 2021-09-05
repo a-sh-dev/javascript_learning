4d413b6a94b03c2c6dc50fdd3c8d7dd6acb2dd2e
a-sh <iam.angie@ymail.com>
2021-09-05 21:54:43 +1000

    Add modify_existing_els to DOM lesson

---
+++ _lectures/dom/script.js
@@ -0,0 +1,50 @@
+
+function addEventListeners() {
+  // Don't Change this function.
+  let darkModeButton = document.getElementById("dark-mode")
+  let lightModeButton = document.getElementById("light-mode")
+  // When the buttons are clicked, they call the respective function
+  darkModeButton.addEventListener('click', changeToDarkMode)
+  lightModeButton.addEventListener('click', changeToLightMode)
+}
+
+function changeToDarkMode() {
+  // Modify the body's class list to change the page to dark mode
+  let body = document.querySelector("body")
+  body.classList.add("darkmode")
+}
+
+function changeToLightMode() {
+  // Modify the body's class list to change the page to light mode
+  let body = document.querySelector("body")
+  body.classList.remove("darkmode")
+}
+
+function changeBulletsToSquares() {
+  // change the bullet cirlces to squares
+  let ul = document.querySelector("ul")
+  ul.style.listStyleType = "square"
+}
+
+function changeFontSizeAndColour() {
+  // Change the body font styles
+  let body = document.querySelector("body")
+  body.setAttribute("style", "font-family: Arial; font-size: 28px");
+  // or
+  // body.style.fontFamily = "Arial"
+  // body.style.fontSize = "28px"
+}
+
+// Do not change this function
+function activity() {
+  addEventListeners()
+  changeBulletsToSquares()
+  changeFontSizeAndColour()
+}
+
+module.exports = {
+  changeToDarkMode,
+  changeToLightMode,
+  changeBulletsToSquares,
+  changeFontSizeAndColour
+}
