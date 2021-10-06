fb362d174b2fdda4af4d1d3a859aaec31a7c74e4
a-sh <iam.angie@ymail.com>
2021-10-06 16:47:48 +1100

    Add mchallenge hackersay.js

--- hacker_speak.js
+++ hacker_speak.js
@@ -11,8 +11,23 @@ hackerSpeak("How about this string?") // returns H3w 0b34t th2s str2ng?
 */
 
 const hackerSpeak = (str) => {
-  const code = {
-    'a': 0, 'e': 1, 'i': 2, 'o': 3, 'u': 4 
-  }
-  
+  const codedStr = ['a', 'e', 'i', 'o', 'u'];
+  // const codedStr = Array.from('aeiou', letter => letter)
+
+  let strArray = str.split('')
+
+  strArray.map((letter, index) => {
+    letter = letter.toLowerCase();
+    // * SPLICE params: the index value to remove, how many elements, replace with...
+    if (codedStr.includes(letter)) {
+      strArray.splice(index, 1, codedStr.indexOf(letter))
+    } 
+  })
+
+  return strArray.join('');
+
 }
+
+console.log(hackerSpeak('Ha ha ha'));
+console.log(hackerSpeak('How about this string?'));
+console.log(hackerSpeak('A bit of a challenge!'));
