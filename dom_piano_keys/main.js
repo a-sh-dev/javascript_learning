a86d12101e9426ece05106ba7780e85801612b05
a-sh <iam.angie@ymail.com>
2021-11-26 21:21:00 +1100

    Add Piano Keys DOM practice

---
+++ dom_piano_keys/main.js
@@ -0,0 +1,117 @@
+// The keys and notes variables store the piano keys
+const keys = [
+  "c-key",
+  "d-key",
+  "e-key",
+  "f-key",
+  "g-key",
+  "a-key",
+  "b-key",
+  "high-c-key",
+  "c-sharp-key",
+  "d-sharp-key",
+  "f-sharp-key",
+  "g-sharp-key",
+  "a-sharp-key"
+];
+
+const notes = [];
+
+keys.forEach((key) => {
+  notes.push(document.getElementById(key));
+});
+
+// A function that change the color of the keys
+const keyPlay = (event) => {
+  event.target.style.backgroundColor = "pink";
+};
+
+const keyReturn = (event) => {
+  event.target.style.backgroundColor = "";
+};
+
+// A function with event handler properties
+let eventHandler = (note) => {
+  note.onmousedown = () => keyPlay(event);
+  note.onmouseup = () => keyReturn(event);
+};
+
+// A loop that runs the array elements through the function
+notes.forEach(eventHandler);
+
+// These variables store the buttons that progress the user through the lyrics
+let nextOne = document.getElementById("first-next-line");
+let nextTwo = document.getElementById("second-next-line");
+let nextThree = document.getElementById("third-next-line");
+let startOver = document.getElementById("fourth-next-line");
+
+// This variable stores the '-END' lyric element
+let lastLyric = document.getElementById("column-optional");
+
+// These statements are "hiding" all the progress buttons, but the first one
+nextTwo.hidden = true;
+nextThree.hidden = true;
+startOver.hidden = true;
+
+// Event handler property and function for the first progress button
+nextOne.onclick = () => {
+  nextTwo.hidden = false;
+  nextOne.hidden = true;
+  document.getElementById("letter-note-five").innerHTML = "D";
+  document.getElementById("letter-note-six").innerHTML = "C";
+};
+
+// Event handler property and function for the second progress button
+nextTwo.onclick = () => {
+  nextThree.hidden = false;
+  nextTwo.hidden = true;
+  // changes on lyrics
+  document.getElementById("word-five").innerHTML = "DEAR";
+  document.getElementById("word-six").innerHTML = "FRI-";
+  lastLyric.style.display = "inline-block";
+  // changes on musical notes
+  document.getElementById("letter-note-three").innerHTML = "G";
+  document.getElementById("letter-note-four").innerHTML = "E";
+  document.getElementById("letter-note-five").innerHTML = "C";
+  document.getElementById("letter-note-six").innerHTML = "B";
+};
+
+// Event handler property and function for the third progress button
+nextThree.onclick = () => {
+  startOver.hidden = false;
+  nextThree.hidden = true;
+  // changes on lyrics when clicked
+  document.getElementById("word-one").innerHTML = "HAP-";
+  document.getElementById("word-two").innerHTML = "PY";
+  document.getElementById("word-three").innerHTML = "BIRTH";
+  document.getElementById("word-four").innerHTML = "DAY";
+  document.getElementById("word-five").innerHTML = "TO";
+  document.getElementById("word-six").innerHTML = "YOU!";
+  // changes on musical notes when clicked
+  document.getElementById("letter-note-one").innerHTML = "F";
+  document.getElementById("letter-note-two").innerHTML = "F";
+  document.getElementById("letter-note-three").innerHTML = "E";
+  document.getElementById("letter-note-four").innerHTML = "C";
+  document.getElementById("letter-note-five").innerHTML = "D";
+  document.getElementById("letter-note-six").innerHTML = "C";
+  // hide last lyric
+  lastLyric.style.display = "none";
+};
+
+// Event handler property and function for the startOver button
+startOver.onclick = function () {
+  nextOne.hidden = false;
+  startOver.hidden = true;
+  document.getElementById("word-one").innerHTML = "HAP-";
+  document.getElementById("letter-note-one").innerHTML = "G";
+  document.getElementById("word-two").innerHTML = "PY";
+  document.getElementById("letter-note-two").innerHTML = "G";
+  document.getElementById("word-three").innerHTML = "BIRTH-";
+  document.getElementById("letter-note-three").innerHTML = "A";
+  document.getElementById("word-four").innerHTML = "DAY";
+  document.getElementById("letter-note-four").innerHTML = "G";
+  document.getElementById("word-five").innerHTML = "TO";
+  document.getElementById("letter-note-five").innerHTML = "C";
+  document.getElementById("word-six").innerHTML = "YOU!";
+  document.getElementById("letter-note-six").innerHTML = "B";
+};
---
+++ dom_piano_keys/readme.md
@@ -0,0 +1,9 @@
+# Piano Keys
+
+_From CodeCademy – Building Interactive Javascript Website_
+
+You’re a web developer who has been hired by a music education company. This client wants you to create an interactive game to help their beginner-level piano students study. Create a piano player with DOM events in JavaScript!
+
+Result:
+
+![outcome preview](./docs/cc_piano.gif)
